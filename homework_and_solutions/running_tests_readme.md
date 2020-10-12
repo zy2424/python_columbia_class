53c48af645634ea8a3acced46fb18588d31cf3e0
Eric Schles <ericschles@gmail.com>
2020-10-12 18:46:19 -0400

    adding examples and test suite instructions

---
+++ homework_and_solutions/running_tests_readme.md
@@ -0,0 +1,13 @@
+# Running the Test Suite
+
+You will run the test suite with `pytest`
+
+To install simply do:
+
+`sudo python -m pip install pytest` 
+
+from the terminal.
+
+To run pytest simply do:
+
+`python -m pytest name_of_test_file.py`
---
+++ homework_and_solutions/test_example_homework.py
@@ -0,0 +1,18 @@
+import example_homework
+import example_homework_part_two
+
+def example_answer():
+    value = 0
+    return value
+
+def test_example():
+    value = example_answer()
+    assert example_homework.value == value
+
+def example_answer_part_two():
+    result = 0
+    return result
+
+def test_example_part_two():
+    result = example_answer_part_two()
+    assert example_home_part_two.result == result
